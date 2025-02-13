43ce9f4486ae08868cb229e5c886584c118b5abc
lfwj <2206649499@qq.com>
2025-02-13 20:38:14 +0800

    Update stretch

--- simulator/robots/stretch.py
+++ simulator/robots/stretch.py
@@ -3,7 +3,7 @@ from simulator.core.config import RobotConfig
 from simulator.core.register import registry
 
 @registry.register_robot(name="Stretch")
-class Gen3_robotiq85(BaseRobot):
+class Stretch(BaseRobot):
     def __init__(self, robot_config:RobotConfig):
         super.__init__(robot_config)
         self.prim_path = "/World/Robot/Stretch" if self.prim_path==None else robot_config.prim_path
@@ -12,18 +12,55 @@ class Gen3_robotiq85(BaseRobot):
     
     @property
     def robot_name(self) -> str:
+        '''
+        robot's name, e.g. "Stretch", "Gen3_robotiq85"
+        '''
         return "Stretch"
     
     @property
     def robot_type(self) -> str:
+        '''
+        robot's type, e.g. "Wheel", "Leg", "Arm", "Drone"
+        '''
         return "Wheel"
     
     @property
     def observation(self):
+        '''
+        Get the observation of the robot from the sensor
+        '''
         pass
     
     @property
     def state(self):
+        '''
+        Get the state of the robot, e.g. position, pose
+        '''
+        pass
+    
+    @property
+    def get_wheel_names(self) -> list[str]:
+        '''
+        Get the name of the Stretch's wheels
+        '''
+        return ["joint_left_wheel", "joint_right_wheel"]
+    
+    @property
+    def get_arm_names(self) -> list[str]:
+        '''
+        Get the name of the Stretch's arm joints
+        '''
+        return ["joint_lift",
+                "joint_arm_l3",
+                "joint_arm_l2",
+                "joint_arm_l1",
+                "joint_arm_l0",
+                "joint_wrist_yaw",
+                "joint_wrist_pitch",
+                "joint_wrist_roll",]
+        
+    
+    def init(self):
         pass
     
     
@@ -41,12 +78,6 @@ class Gen3_robotiq85(BaseRobot):
         else:
             pass
 
-    def get_joint_names(self) -> list[str]:
-        pass
-
-    def init(self):
-        pass
-    
     def reset(self):
         pass
     
@@ -56,10 +87,7 @@ class Gen3_robotiq85(BaseRobot):
     def obj_release(self):
         pass
     
-    # friction control
-    def move_to_target_velocity(self, target):
-        pass
-    
+
     # position control
     def move_to_target_velocity(self, target):
         pass
