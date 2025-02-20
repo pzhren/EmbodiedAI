61c7ebf0b506e52a0092ad48e60a4ddcd1336354
lfwj <2206649499@qq.com>
2025-02-20 20:38:20 +0800

    update position control

--- simulator/controllers/position_controller.py
+++ simulator/controllers/position_controller.py
@@ -1,6 +1,8 @@
 from simulator.core.config import ControllerConfig
 from simulator.core.register import registry
 from simulator.core.controller import BaseController
+import numpy as np
+import math
 
 @registry.register_controller
 class PositionController(BaseController):
@@ -12,5 +14,55 @@ class PositionController(BaseController):
         super().__init__(config)
     
 
-    def get_action(self) -> np.ndarray:
+    def get_action(self,str) -> np.ndarray:
+        '''
+        w: Move forward
+        s: Move backward
+        a: Turn left
+        d: Turn right
+        '''
+        
         return np.array([0.0, 0.0, 0.0, 0.0, 0.0, 0.0])
+    
+    
+    def forward_by_mode(self):
+        pass
+    
+    
+    def turn_by_mode(self):
+        pass
+    
+    
+    def move_by_target(self, target, world, orientation=False):
+        """
+        Move the robot to a target position and orientation.
+        :param target: Target position (x, y) and orientation (w) in radians.
+        :param world: Simulation world.
+        :param orientation: Whether to adjust the orientation in the process.
+        """
+        
+        pass
+    
+    
+    def move_by_mode(self):
+        pass
+    
+    def trans_pos(self):
+        pass
+    
+    
+    def trans2pi(self,num):
+        """
+        Convert angle difference to the range [-pi, pi].
+        :param num: Angle difference.
+        :return: Angle within the range [-pi, pi].
+        """
+        while num < -math.pi or num > math.pi:
+            if num < -math.pi:
+                num += 2*math.pi
+            else:
+                num -= 2*math.pi
+        return num
+    
+    
+    
