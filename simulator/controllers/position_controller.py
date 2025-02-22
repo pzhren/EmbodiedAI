138fb50e85747958e8fd5f2227b503239ec5834c
lfwj <2206649499@qq.com>
2025-02-22 17:56:32 +0800

    update postioncontrol

--- simulator/controllers/position_controller.py
+++ simulator/controllers/position_controller.py
@@ -1,8 +1,13 @@
 from simulator.core.config import ControllerConfig
 from simulator.core.register import registry
 from simulator.core.controller import BaseController
-import numpy as np
+
+from transformations import euler_from_quaternion, quaternion_from_euler
+from omni.isaac.core.robots import Robot
+
 import math
+import numpy as np
+
 
 @registry.register_controller
 class PositionController(BaseController):
@@ -10,59 +15,76 @@ class PositionController(BaseController):
     input_limit: str = "default"
     output_limit: str = "default"
     name: str = "position_controller"
+    
+    
     def __init__(self, config:ControllerConfig):
         super().__init__(config)
     
 
-    def get_action(self,str) -> np.ndarray:
-        '''
-        w: Move forward
-        s: Move backward
-        a: Turn left
-        d: Turn right
-        '''
+    def get_action(self, command: str, length=0.02, angle_yaw=0.02) -> np.ndarray:
+        """
+        Generate action based on movement command.
         
-        return np.array([0.0, 0.0, 0.0, 0.0, 0.0, 0.0])
-    
-    
-    def forward_by_mode(self):
-        pass
-    
-    
-    def turn_by_mode(self):
-        pass
-    
-    
-    def move_by_target(self, target, world, orientation=False):
+        Args:
+            command: Movement command (w/s/a/d)
+            length: Linear displacement (default: 0.02)
+            angle_yaw: Angular displacement (default: 0.02 rad)
+            
+        Returns:
+            np.ndarray: Target position and yaw angle
         """
-        Move the robot to a target position and orientation.
-        :param target: Target position (x, y) and orientation (w) in radians.
-        :param world: Simulation world.
-        :param orientation: Whether to adjust the orientation in the process.
-        """
+        position, _, _, yaw = self.trans_pos()
+        x_dir = np.array([math.cos(yaw), math.sin(yaw), 0])
         
-        pass
+        if command == 'w':
+            target_pos = position + length * x_dir
+        elif command == 's':
+            target_pos = position - length * x_dir
+        else:
+            target_pos = position
+        
+        new_yaw = yaw
+        if command == 'a':
+            new_yaw = (yaw - angle_yaw) % (2 * math.pi)
+        elif command == 'd':
+            new_yaw = (yaw + angle_yaw) % (2 * math.pi)
+
+        return np.array([target_pos, new_yaw])
     
     
-    def move_by_mode(self):
-        pass
-    
-    def trans_pos(self):
-        pass
-    
-    
-    def trans2pi(self,num):
+    def trans_pos(self, robot)-> tuple:
         """
-        Convert angle difference to the range [-pi, pi].
-        :param num: Angle difference.
-        :return: Angle within the range [-pi, pi].
+        Get robot's current position and orientation.
+        
+        Args:
+            robot: Robot instance
+            
+        Returns:
+            tuple: (position, roll, pitch, yaw)
         """
-        while num < -math.pi or num > math.pi:
-            if num < -math.pi:
-                num += 2*math.pi
+        robot_pos = Robot(robot)
+        position, quaternion  = robot_pos.get_world_pose()
+        roll, pitch, yaw = euler_from_quaternion(quaternion)
+        return position, roll, pitch, yaw 
+    
+    
+    @staticmethod
+    def normalize_angle(angle: float)-> float:
+        """
+        Normalize angle to [-π, π] range.
+        
+        Args:
+            angle: Input angle in radians
+            
+        Returns:
+            float: Normalized angle
+        """
+        while angle < -math.pi or angle > math.pi:
+            if angle < -math.pi:
+                angle += 2*math.pi
             else:
-                num -= 2*math.pi
-        return num
+                angle -= 2*math.pi
+        return angle
     
     
     
