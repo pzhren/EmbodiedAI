55f4f4fd9b357f8fadd909d27438170caeebebc6
Linmin1209 <1091635072@qq.com>
2025-05-07 20:40:26 +0800

    Create docker_start.sh

---
+++ scripts/docker_start.sh
@@ -0,0 +1,12 @@
+docker run --name embodiedai --entrypoint bash --runtime=nvidia -it --gpus all -e "ACCEPT_EULA=Y" --network=host \
+    -e "PRIVACY_CONSENT=Y" \
+    -e MESA_GL_VERSION_OVERRIDE=4.6 \
+    -v ~/docker/isaac-sim/cache/kit:/isaac-sim/kit/cache:rw \
+    -v ~/docker/isaac-sim/cache/ov:/root/.cache/ov:rw \
+    -v ~/docker/isaac-sim/cache/pip:/root/.cache/pip:rw \
+    -v ~/docker/isaac-sim/cache/glcache:/root/.cache/nvidia/GLCache:rw \
+    -v ~/docker/isaac-sim/cache/computecache:/root/.nv/ComputeCache:rw \
+    -v ~/docker/isaac-sim/logs:/root/.nvidia-omniverse/logs:rw \
+    -v ~/docker/isaac-sim/data:/root/.local/share/ov/data:rw \
+    -v ~/docker/isaac-sim/documents:/root/Documents:rw \
+    twilighted/embodiedai:v2
